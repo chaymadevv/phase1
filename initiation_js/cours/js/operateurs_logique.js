ae57522c2d17b76e0f9d911b79ebe0356d0c9854
chems <chayma.kilic@lepoles.org>
2024-06-03 16:39:58 +0200

    03_06

---
+++ initiation_js/cours/js/operateurs_logique.js
@@ -0,0 +1,21 @@
+alert("chayma" + <br>);
+    
+var prenom = window.prompt("Insserez votre prenom :");
+    var prenom2 = "Nolann";
+    var prenom3 = "Gustave";
+    var age =19;
+    var anneeCourante = 2024;
+    //document.write(prenom + </br>
+    
+    document.write( prenom);
+    console.log(prenom);
+
+    if (prenom===prenom2){
+        document.write(" est egale a prenom2 !");
+    } else{
+        document.write(prenom + " est egale a " + prenom2 + "!");
+    }
+    
+    if (prenomLogin === prenom && ageLogin === monAge) {
+
+    }
