f56cd3d938392e6851e98cf2d3611979598cd24a
chems <chayma.kilic@lepoles.org>
2024-06-07 17:00:25 +0200

    07062024

--- initiation_js/cours/js/boucles.js
+++ initiation_js/cours/js/boucles.js
@@ -24,6 +24,9 @@ document.write("<h3>Jusqu'à 2000e, il me faut : " + temps + " mois</h3");
 
 // exo
 var majoriteFr = 18;
+// Nous allons effectuer plusieurs actions en une seule syntaxe:
+// 1 declarer une variable age
+// 2 convertir la valeur de retour
 var age = parseInt(window.prompt("Quel âge as-tu?"));
 console.log(typeof age);
 
@@ -34,3 +37,20 @@ if (age >= 18) {
   window.open("http://www.darty.com/");
 }
 
+ //Exercice
+// 1 on utilise le IF pour verifier l'age de linternaute.
+// tant que l'utilisateur ne rentre pas une nombre entre 3 et 120 on lui redemande sont age
+// a) s'il est majeur je lui souhaite la bienvenue
+// b) s'il est mineur je
+// 1 - lui signal et
+// 2- le renvoie vers un autre Site
+
+var age window.prompt("Inserez votre age !");
+var ageMin = 3;
+var ageMax = 120;
+var majoriteFr = 18;
+
+while (age <= ageMin || age >= ageMax) {
+  age = parseInt(window.prompt("Inserez votre age!"));
+}
+if(age>= majoriteFr && age <= ageMax)
---
+++ initiation_js/cours/js/tableau.js
@@ -0,0 +1,13 @@
+var fruits;
+fruits = ["Apple", "banana"];
+var tailleTab = fruits.length;
+console.log(tailleTab);
+console.log(fruits[0]);
+console.log(fruits [tailleTab-1])
+
+var couleurs;
+var couleurs =["pink , black , purple"];
+var tailleCouleurs = couleurs.length;
+console.log(tailleTab);
+
+
