8a8249c53648d03c3dd2a8da6aa5ba4ba5d1e5a3
chems <chayma.kilic@lepoles.org>
2024-06-13 16:57:58 +0200

    13062024

---
+++ initiation_js/exo_js/js/nb_jours.js
@@ -0,0 +1,52 @@
+var mois = parseInt(window.prompt("Insérez un nombre entre 1 et 12 ..."))
+while (isNaN(mois < 1 || mois > 12)) {
+    mois = Number( window.prompt("Insérezz une valeur numerique correct en 1 et 12"))
+}
+
+if ((mois % 2 == 0 || mois != 2 || mois != 8)) {
+    alert("ce mois contient 30 jours");
+} elsev if (mois % 2 == 1 || mois == 8) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // mois = isNaN(mois);
+
+// var janvier = 1;
+// var fevrier = 2; 
+// var mars = 3; 
+// var avril = 4; 
+// var mai = 5;
+// var juin = 6; 
+// var juillet = 7; 
+// var aout = 8; 
+// var septembre = 9;
+// var octobre = 10;
+// var novembre = 11;
+// var decembre = 12; 
